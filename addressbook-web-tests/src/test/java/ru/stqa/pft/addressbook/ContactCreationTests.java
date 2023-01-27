15bafd07f74f530f93ae9b66e570270fbdc69749
maryiabarysavets <120658451+maryiabarysavets@users.noreply.github.com>
2023-01-27 23:23:52 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге 1. Выделила вспомогательные методы 2. Сделала методы параметризованными - Добавлены параметры username, password, FirstName, LastName, Mobile, Email, Address в методы 3.Создан вспомогательный класс ContactData
    (удалила слово Tests из public void testContactCreationTests()

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -32,7 +32,7 @@ public class ContactCreationTests {
   }
 
   @Test
-  public void testContactCreationTests() {
+  public void testContactCreation() {
     createNewContact();
     fillContactForm(new ContactData("Maryia", "Barysavets", "375336514233", "mariaborisovets@gmail.com", "Minsk"));
     submit();
