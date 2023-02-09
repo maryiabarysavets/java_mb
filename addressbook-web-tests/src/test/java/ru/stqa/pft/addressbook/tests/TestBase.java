04e9be39b883257912547f253d91742d11b1938d
maryiabarysavets <120658451+maryiabarysavets@users.noreply.github.com>
2023-02-09 14:34:43 +0300

    Лекция 6.11. Протоколирование (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,20 +5,35 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
+
+//  протолирование (лог-файлы)
+//  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app =
           new ApplicationManager(System.getProperty("browser",Browser.CHROME.browserName()));
 
 
   @BeforeSuite(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void setUp() throws IOException {
     app.init();
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void tearDown()  {
     app.stop();
   }
 
+//  протолирование (лог-файлы)
+//  @BeforeMethod
+//  public void logTestStart(Method m, Object[] p){
+//    logger.info("Start test"+ m.getName() + "with parameters" + Arrays.asList(p));
+//  }
+
+//  @AfterMethod
+//  public void logTestStop(Method m, Object[] p){
+//    logger.info("Stop test" + m.getName()+ "with parameters" + Arrays.asList(p));
+//  }
 }
