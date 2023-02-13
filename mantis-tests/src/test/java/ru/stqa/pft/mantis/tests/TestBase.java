590d8e406477982dc37ccaeec6d470bae4aa2ccc
maryiabarysavets <120658451+maryiabarysavets@users.noreply.github.com>
2023-02-13 23:09:17 +0300

    Задание №19: Реализовать интеграцию тестов с баг-трекером MantisBT (в тест добавлен skipIfNotFixed)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.mantis.tests;
 
+
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.Browser;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
-import java.io.File;
+import javax.xml.rpc.ServiceException;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 public class TestBase {
 
@@ -23,7 +30,7 @@ public class TestBase {
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
-   // app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
+    // app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
   }
 
   @AfterSuite(alwaysRun = true)
@@ -32,4 +39,21 @@ public class TestBase {
     app.stop();
   }
 
+
+  public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+    MantisConnectPortType mc = app.soap().getMantisConnect();
+    IssueData bug = mc.mc_issue_get("administrator", "root", BigInteger.valueOf(issueId));
+    String issueStatus = bug.getStatus().getName();
+    if (issueStatus.equals("resolved")) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  public void skipIfNotFixed(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
 }
